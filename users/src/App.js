7953bf4b3df27ab12e73adb0f6210d3b6181a6c8
Na1jel <skolbik@inbox.ru>
2021-09-09 17:49:44 +0300

    второе

--- users/src/App.js
+++ users/src/App.js
@@ -2,23 +2,18 @@ import React from 'react';
 import axios from 'axios'
 import Table from 'react-bootstrap/Table'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  NavLink
-} from "react-router-dom";
-import OneUser from './OneUser';
+
 
 
 class Client extends React.Component{
   constructor(props){
     super(props)
-    this.state ={users:[], page:1}
+    this.state ={users:[], page:1, sort:[], search:[]}
     this.createUser = this.createUser.bind(this)
     this.nextuser = this.nextuser.bind(this)
     this.backUser = this.backUser.bind(this)
+    this.sort = this.sort.bind(this)
+    this.poisk = this.poisk.bind(this)
   }
 
   createUser(e){
@@ -28,7 +23,6 @@ class Client extends React.Component{
       if(res.status ===200){
         this.setState({users:res.data})
       }
-      console.log(this.state.users)
     })
   }
 
@@ -43,21 +37,54 @@ class Client extends React.Component{
   backUser(e){
     const page = this.state.page - 1
     this.setState({page: page})
-  
       return this.createUser()
-
   }
 
+  sort(e){
+    const tag = e.target.tagName
+    let sort = this.state.users
+    console.log(tag)
+   if(tag ){
+    sort.reverse()
+   }else{
+    sort.sort((a1, b1)=>{
+      if(a1.name < b1.name){
+        return -1
+      }
+      if(a1.name > b1.name){
+        return 1
+      }
+      return 0
+    })
+    
+   }
+   this.setState({users:sort})
+  }
 
+  poisk(e){
+    let search = this.state.users.map(u=> u.name);
+    let a = search.join()
+    let p = a.includes(e.target.value)
+    if(p){
+      search.filter(n=>n)
+    }
+    this.setState({search:search})
+    console.log(p)
+  }
 
   render(){
     return(
-      <div>
+      <div className="container">
         
       <button onClick={this.createUser}>Create user </button>
+        <br></br>
+        <br></br>
+      <input type='text' onChange={this.poisk} ></input>
+      <br></br>
+        <br></br>
       <Table striped bordered hover>
   <thead>
-    <tr>
+    <tr onClick={this.sort}>
       <th>First Name</th>
       <th>phone</th>
       <th>website</th>
@@ -65,9 +92,9 @@ class Client extends React.Component{
     </tr>
   </thead>
   <tbody>
-  {this.state.users.map(u=><tr>
+  {this.state.users.map(u=><tr key={u.name}>
     <td>
-    <NavLink  className="active" to='/OneUser' > {u.name} </NavLink > 
+    {u.name} 
     </td>
     <td>
       {u.phone}
@@ -85,9 +112,8 @@ class Client extends React.Component{
   </tbody>
 </Table>
 <button onClick={this.backUser}>Back</button>
-
 <button onClick={this.nextuser}>Next</button>
-<OneUser phone={this.state.users.map(u=> u.phone)}/>
+
       </div>
      
     )
--- users/src/index.js
+++ users/src/index.js
@@ -1,27 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Client from './App';
-import OneUser from './OneUser'
-
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
 
 
 ReactDOM.render(
   <React.StrictMode>
-    
-     <Router>
-     <Client />
-       <Switch>
-          <Route exact path="/OneUser" >
-          <OneUser />   
-          </Route>
-       </Switch>
-    </Router>
+      <Client />
+     
   </React.StrictMode>,
   document.getElementById('root')
 );
